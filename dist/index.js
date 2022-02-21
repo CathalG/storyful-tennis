db7e3087c055668827aa5f46c0c79df4cc292cc2
CathalG <cathalgrennan@gmail.com>
2022-02-21 12:48:02 +0000

    Added basic game/scoring functionality and scoring tests

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,47 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getScore = getScore;
+exports.getScoreAsString = getScoreAsString;
+exports.playerOneScores = playerOneScores;
+exports.playerTwoScores = playerTwoScores;
+exports.startGame = startGame;
+let score; //Start new game (reset score and win condition)
+
+function startGame() {
+  score = [0, 0];
+} //Player one scores a point 
+
+
+function playerOneScores() {
+  score[0]++;
+} //Player Two scores a point
+
+
+function playerTwoScores() {
+  score[1]++;
+} //return individual score as corresponding tennis score 
+
+
+function getScoreAsString(score) {
+  switch (score) {
+    case 0:
+      return "Love";
+
+    case 1:
+      return "15";
+
+    case 2:
+      return "30";
+
+    case 3:
+      return "40";
+  }
+} //return current score as tennis score
+
+
+function getScore() {
+  return `${getScoreAsString(score[0])} - ${getScoreAsString(score[1])}`;
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 
+let score;
 
+
+//Start new game (reset score and win condition)
+export function  startGame() {
+    score = [0, 0];
+}
+
+  //Player one scores a point 
+  export function playerOneScores() {
+    score[0]++;
+  }
+  
+  //Player Two scores a point
+  export function playerTwoScores() {
+    score[1]++;
+  }
+
+  //return individual score as corresponding tennis score 
+  export function getScoreAsString(score) {
+    switch (score) {
+      case 0:
+        return "Love";
+      case 1:
+        return "15";
+      case 2:
+        return "30";
+      case 3:
+        return "40";
+    }
+  }
+
+  //return current score as tennis score
+  export function getScore() {
+    return `${getScoreAsString(score[0])} - ${getScoreAsString(score[1])}`;
+  }
